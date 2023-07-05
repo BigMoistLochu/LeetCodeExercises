2683546fcbdfb0d0402e03243659ce97b05dacb7
BigMoistLochu <krok.konrad1@gmail.com>
2023-07-05 23:30:19 +0200

    add MissingNumberEasy class to solve in future

---
+++ src/main/java/org/example/ArraysAndHashing/MissingNumberEasy.java
@@ -0,0 +1,11 @@
+package org.example.ArraysAndHashing;
+
+public class MissingNumberEasy {
+    //Given an array nums containing n distinct numbers in the range [0, n],
+    // return the only number in the range that is missing from the array.
+
+    public int missingNumber(int[] nums) {
+
+        return 0;
+    }
+}
