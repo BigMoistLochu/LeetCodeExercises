b7b5ef29a65ed04558d2d38a33b1add462cb4de5
BigMoistLochu <krok.konrad1@gmail.com>
2023-07-06 00:39:19 +0200

    more simpler solution of MissingNumberEasy Complexity O(n)

--- src/main/java/org/example/ArraysAndHashing/MissingNumberEasy.java
+++ src/main/java/org/example/ArraysAndHashing/MissingNumberEasy.java
@@ -12,29 +12,20 @@ public class MissingNumberEasy {
     //Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9].
     // 8 is the missing number in the range since it does not appear in nums.
     public int missingNumber(int[] nums) {
-
-        //tablice rozbijamy na hashmape i przechodzimy przez klucze
-        //wiec bedziemy miec unikalne wartosci
-        //potem wystarczy zrobic pentle i iterowac wywolujac funkcje map.containsKeyI()
-        Map<Integer,Integer> map = new HashMap<>();
-        int missingNumber = -1;
-
-        for (int i = 0; i < nums.length; i++) {
-
-            if(!map.containsKey(nums[i]))
+            //simpler solution than before
+            int arr[] = new int[nums.length + 1];
+            for(int i=0;i<nums.length;i++)
             {
-                map.put(nums[i],i);
+                arr[nums[i]] = 1;
             }
-        }
-        //mapa stworzona
-        for (int i = 0; i < nums.length+1; i++) {
-          if(!map.containsKey(i))
-          {
-              missingNumber = i;
-          }
-        }
+            for(int i=0;i<arr.length;i++)
+            {
+                if(arr[i] == 0)
+                {
+                    return i;
+                }
+            }
 
-
-        return missingNumber;
+        return -1;
     }
 }
