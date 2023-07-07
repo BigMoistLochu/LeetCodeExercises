621d3ffb0a031a1a8b7f328be7d1effc18cacd5a
BigMoistLochu <krok.konrad1@gmail.com>
2023-07-07 06:00:22 +0200

    add another problem to the solve in the future

---
+++ src/main/java/org/example/ArraysAndHashing/MergeStringsAlternatelyEasy.java
@@ -0,0 +1,13 @@
+package org.example.ArraysAndHashing;
+
+
+//You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+//
+//Return the merged string.
+public class MergeStringsAlternatelyEasy {
+
+    public String mergeAlternately(String word1, String word2) {
+
+        return "Solution in progress";
+    }
+}
