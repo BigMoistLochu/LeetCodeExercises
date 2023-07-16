72cd011a1071f5b4b5794036e15f4e059c3f9be4
BigMoistLochu <krok.konrad1@gmail.com>
2023-07-16 21:48:58 +0200

    add KidsWithTheGreatestNumberOfCandiesEasy problem to solve

---
+++ src/main/java/org/example/ArraysAndHashing/KidsWithTheGreatestNumberOfCandiesEasy.java
@@ -0,0 +1,12 @@
+package org.example.ArraysAndHashing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KidsWithTheGreatestNumberOfCandiesEasy {
+
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+        return new ArrayList<>(List.of(true));
+    }
+}
