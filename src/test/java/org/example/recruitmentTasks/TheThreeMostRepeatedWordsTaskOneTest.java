254d397ae3cc3214033da97d94b1577b33284eff
BigMoistLochu <krok.konrad1@gmail.com>
2023-08-05 03:43:43 +0200

    add test

--- src/test/java/org/example/recruitmentTasks/TheThreeMostRepeatedWordsTaskOneTest.java
+++ src/test/java/org/example/recruitmentTasks/TheThreeMostRepeatedWordsTaskOneTest.java
@@ -16,4 +16,14 @@ class TheThreeMostRepeatedWordsTaskOneTest {
         assertEquals("aba",theThreeMostRepeatedWordsTaskOne.getThreeMostRepeatedWords(ape)[1]);
         assertEquals("d",theThreeMostRepeatedWordsTaskOne.getThreeMostRepeatedWords(ape)[2]);
     }
+
+    @Test
+    void getThreeMostRepeatedWordsShouldReturnFalse() {
+
+        String ape = "aba d d k cs aba i aba u d aba i i i i i x x x x x x x x x";
+
+        assertEquals("x",theThreeMostRepeatedWordsTaskOne.getThreeMostRepeatedWords(ape)[0]);
+        assertEquals("i",theThreeMostRepeatedWordsTaskOne.getThreeMostRepeatedWords(ape)[1]);
+        assertEquals("aba",theThreeMostRepeatedWordsTaskOne.getThreeMostRepeatedWords(ape)[2]);
+    }
 }
