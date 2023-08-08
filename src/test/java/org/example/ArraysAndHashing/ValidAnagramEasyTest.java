abb39a76f22d7f19eb59e4081d10862af070d242
BigMoistLochu <krok.konrad1@gmail.com>
2023-08-08 22:03:51 +0200

    add test to Anagram

--- src/test/java/org/example/ArraysAndHashing/ValidAnagramEasyTest.java
+++ src/test/java/org/example/ArraysAndHashing/ValidAnagramEasyTest.java
@@ -2,6 +2,11 @@ package org.example.ArraysAndHashing;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValidAnagramEasyTest {
@@ -24,6 +29,9 @@ class ValidAnagramEasyTest {
 
     @Test
     void isAnagramShouldReturnFalseWithAnotherParam() {
+
+
+
 
 
         assertFalse(validAnagramEasy.isAnagram("apes","sepaaj"));
