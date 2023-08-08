76445ffc0f2c86dc259c8b3cb52d3f5b0fcf1494
BigMoistLochu <krok.konrad1@gmail.com>
2023-08-08 22:10:42 +0200

    add test MissingNumberEasyTest

--- src/test/java/org/example/ArraysAndHashing/MissingNumberEasyTest.java
+++ src/test/java/org/example/ArraysAndHashing/MissingNumberEasyTest.java
@@ -12,4 +12,11 @@ class MissingNumberEasyTest {
         int[] list = {9,6,4,2,3,5,7,0,1};
         assertEquals(8,missingNumberEasy.missingNumber(list));
     }
+
+    @Test
+    void missingNumberShouldReturnMissingNumberIs2() {
+        int[] list = {9,6,4,8,3,5,7,0,1};
+
+        assertEquals(2,missingNumberEasy.missingNumber(list));
+    }
 }
