d5fa9330684d147048528349326a6b298d8d7c37
BigMoistLochu <krok.konrad1@gmail.com>
2023-08-23 18:29:06 +0200

    remove borderplate

--- src/main/java/org/example/LongestCommonPrefixEasy.java
+++ src/main/java/org/example/LongestCommonPrefixEasy.java
@@ -2,18 +2,7 @@ package org.example;
 
 public class LongestCommonPrefixEasy {
 
-    //*Write a function to find the longest common prefix string amongst an array of strings.
-    //
-    //If there is no common prefix, return an empty string "".
 
-    //EXAMPLE 1
-    //Input: strs = ["flower","flow","flight"]
-    //Output: "fl"
-
-    //EXAMPLE 2
-    //Input: strs = ["dog","racecar","car"]
-    //Output: ""
-    //Explanation: There is no common prefix among the input strings.
 
     public String longestCommonPrefix(String[] strs) {
 
