96d07860e71b31624da52b96bf7147dd376f7986
BigMoistLochu <krok.konrad1@gmail.com>
2023-10-05 20:28:55 +0200

    Add solution for  task(IsSubsequence)

--- src/main/java/org/example/ArraysAndHashing/IsSubsequence.java
+++ src/main/java/org/example/ArraysAndHashing/IsSubsequence.java
@@ -9,6 +9,25 @@ public class IsSubsequence {
     // (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
     public boolean isSubsequence(String s, String t) {
 
+        int i=0,j=0;
+        int n=t.length();
+        int m=s.length();
+        char ss[]=s.toCharArray();
+        char tt[]=t.toCharArray();
+
+        if(m<1)
+            return true;
+
+        while(i<n){
+            if(tt[i]==ss[j]){
+                j++;
+            }
+            i++;
+
+            if(j==m)
+                return true;
+        }
+
         return false;
     }
 }
