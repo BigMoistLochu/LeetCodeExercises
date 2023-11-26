fce7639e4b24530d5b4482727873e1adcc7c3237
BigMoistLochu <krok.konrad1@gmail.com>
2023-11-26 23:27:29 +0100

    add solution MoveZeroesEasy

--- src/main/java/org/example/ArraysAndHashing/MoveZeroesEasy.java
+++ src/main/java/org/example/ArraysAndHashing/MoveZeroesEasy.java
@@ -16,5 +16,15 @@ public class MoveZeroesEasy {
     public void moveZeroes(int[] nums) {
         //wstaw -1 tam gdzie sa zera
         //
+        if (nums == null || nums.length == 0) return;
+
+        int insertPos = 0;
+        for (int num: nums) {
+            if (num != 0) nums[insertPos++] = num;
+        }
+
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
+        }
     }
 }
