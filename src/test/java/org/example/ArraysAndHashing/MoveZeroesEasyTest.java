9fc08f066de71eaeadc89d4ea030a99717e4dfbc
BigMoistLochu <krok.konrad1@gmail.com>
2023-11-29 19:34:01 +0100

    add another test to MoveZeroesEasyTest

--- src/test/java/org/example/ArraysAndHashing/MoveZeroesEasyTest.java
+++ src/test/java/org/example/ArraysAndHashing/MoveZeroesEasyTest.java
@@ -18,9 +18,21 @@ public class MoveZeroesEasyTest {
 
     @Test
     public void moveZeroesMethodShouldReturnTwelveOnStart()
-    {   //Input: nums = [0,1,0,3,12]
-        //Output: [1,3,12,0,0]
+    {   //Input: nums = [0,0,0,0,12]
+        //Output: [12,0,0,0,0]
         int[] inputArray = {0,0,0,0,12};
+        int[] outputArray = {12,0,0,0,0};
+
+        MoveZeroesEasy.moveZeroes(inputArray);
+
+        assertArrayEquals(outputArray,inputArray);
+    }
+
+    @Test
+    public void moveZeroesMethodShouldReturnTwelveOnMiddle()
+    {   //Input: nums = [0,0,12,0,0]
+        //Output: [12,0,0,0,0]
+        int[] inputArray = {0,0,12,0,0};
         int[] outputArray = {12,0,0,0,0};
 
         MoveZeroesEasy.moveZeroes(inputArray);
