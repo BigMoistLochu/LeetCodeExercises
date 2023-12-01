7d9383803b4fe57da4945962a0c56107f723842c
BigMoistLochu <krok.konrad1@gmail.com>
2023-12-01 19:24:59 +0100

    remove test for MoveZeroesEasyTest

--- src/test/java/org/example/ArraysAndHashing/MoveZeroesEasyTest.java
+++ src/test/java/org/example/ArraysAndHashing/MoveZeroesEasyTest.java
@@ -40,17 +40,7 @@ public class MoveZeroesEasyTest {
         assertArrayEquals(outputArray,inputArray);
     }
 
-    @Test
-    public void moveZeroesMethodShouldReturnAllZeros()
-    {   //Input: nums = [0,0,0,0,0]
-        //Output: [0,0,0,0,0]
-        int[] inputArray = {};
-        int[] outputArray = {};
 
-        MoveZeroesEasy.moveZeroes(inputArray);
-
-        assertArrayEquals(outputArray,inputArray);
-    }
 
 
 }
